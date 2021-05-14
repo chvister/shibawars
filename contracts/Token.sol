0f89184951a7c3bfe2c4cd68a951ec5e456bde0f
Dominik Krížo <dominik.krizo@doxxbet.sk>
2021-05-14 18:47:06 +0200

    mint random shiba

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -15,6 +15,7 @@ contract Token is ERC721, Ownable {
         uint dexterityGain; // dexterity gain per level
 
         uint level;         // level
+        uint arenaScore;    // score in arena
 
         uint tokenId;       // id of token           
         string name;        // name
@@ -28,32 +29,191 @@ contract Token is ERC721, Ownable {
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
     }
 
-    function mint(string memory name, string memory description, uint strength, uint agility, uint dexterity) public onlyOwner {
-        _tokenDetails[nextId] = Shiba(strength, agility, dexterity, strength / 10, agility / 10, dexterity / 10, 1, 0, name, description);
+    function mint(uint tokenId, uint strength, uint agility, uint dexterity) public onlyOwner {
+        _tokenDetails[nextId] = Shiba(strength * 10, agility * 10, dexterity * 10, strength, agility, dexterity, 1, 0, tokenId, getName(tokenId), getDescription(tokenId));
         /**
         token id: 
-        0: Bojar
-        1: Woofmeister
-        2: Shiba Whale
-        3: OG Shiba
-        4: Shiba Warlord
-        5: Shiba General
-        6: Watchdog
-        7: Doge Killer
-        8: Shiba Inu
-        9: Akita Inu
-        10: Sanshu Inu
-        11: Shiba Pup
-        12: Lucky Doge Pack Gen#1
-        13: Doge Father
-        14: Golden Doge
-        15: Ryoshi
+        0: Bojar da Killa
+        1: Kaya the Wolfmother
+        2: Woofmeister
+        3: Shiba Whale
+        4: OG Shiba
+        5: Shiba Warlord
+        6: Shiba General
+        7: Watchdog
+        8: Doge Killer
+        9: Shiba Inu
+        10: Akita Inu
+        11: Sanshu Inu
+        12: Shiba Pup
+        13: Lucky Doge Pack Gen#1
+        14: Doge Father
+        15: Golden Doge
+        16: Ryoshi
+        17: Power Treat
          */  
         _safeMint(msg.sender, nextId);
         ++nextId;
     }
 
+    function canFight(uint tokenId) public view returns (bool) {
+        uint id =  _tokenDetails[tokenId].tokenId;
+        return id > 1 && id != 17 && id != 13;
+    }
+
+    function getName(uint tokenId) private view returns (string memory) {
+        if (tokenId == 0){
+            return "Bojar da Killa";
+        } else if (tokenId == 1) {
+            return "Kaya the Wolf Mother";
+        } else if (tokenId == 2) {
+            return "WoofMeister";
+        } else if (tokenId == 3) {
+            return "Shiba Whale";
+        } else if (tokenId == 4) {
+            return "OG Shiba";
+        } else if (tokenId == 5) {
+            return "Shiba Warlord";
+        } else if (tokenId == 6) {
+            return "Shiba General";
+        } else if (tokenId == 7) {
+            return "Watchdog";
+        } else if (tokenId == 8) {
+            return "Doge Killer";
+        } else if (tokenId == 9) {
+            return "Shiba Inu";
+        } else if (tokenId == 10) {
+            return "Akita Inu";
+        } else if (tokenId == 11) {
+            return "Sanshu Inu";
+        } else if (tokenId == 12) {
+            return "Shiba Pup";
+        } else if (tokenId == 13) {
+            return "Lucky Doge Pack Gen #1";
+        } else if (tokenId == 14) {
+            return "Doge Father";
+        } else if (tokenId == 15) {
+            return "Golden Doge";
+        } else if (tokenId == 16) {
+            return "Ryoshi";
+        } else if (tokenId == 17) {
+            return "Power Treat";
+        }
+        return "";
+    }
+
+
+    function getDescription(uint tokenId) private view returns (string memory) {
+        if (tokenId == 0) {
+            return "Altough he's not a Shiba, do not mess with him. The warden of order.";
+        } else if (tokenId == 1) {
+            return "She may be cute, but she will get you. Beware, she bites.";
+        } else if (tokenId == 2) {
+            return "The one who has power over all the dogs. We look up to you and believe in you.";
+        } else if (tokenId == 3) {
+            return "The true holders of the ShibArmy.";
+        } else if (tokenId == 4) {
+            return "They were here since the beginning. The true loyal ones.";
+        } else if (tokenId == 5) {
+            return "Altough they may not be recognized, they do lead the ShibArmy forward.";
+        } else if (tokenId == 6) {
+            return "One bark, and they are in the battle.";
+        } else if (tokenId == 7) {
+            return "This is not a shiba. But a small cute doge needs a big strong DOG to defend it.";
+        } else if (tokenId == 8) {
+            return "Put the doge on the leash. Even though the doges hold together, Doge Killer is true to its beliefs.";
+        } else if (tokenId == 9) {
+            return "Just look at it! How can you not want to own them all?";
+        } else if (tokenId == 10) {
+            return "A copy cat? NO! Just another cute Inu family member!";
+        } else if (tokenId == 11) {
+            return "Do you even know this one? If you don't, just look at it!";
+        } else if (tokenId == 12) {
+            return "AWWWWWWWWWWWWWWWWWW";
+        } else if (tokenId == 13) {
+            return "Open for a chance to get a very rare Doge, including the WoofMeister themself.";
+        } else if (tokenId == 14) {
+            return "A friend should always underestimate your virtues and an enemy overestimate your faults.";
+        } else if (tokenId == 15) {
+            return "We all are in it. AND THIS ONE IS GOLDEN!";
+        } else if (tokenId == 16) {
+            return "The one who took us under their wings. Ryoshi.";
+        } else if (tokenId == 17) {
+            return "Feed this to your doge to increase its level.";
+        }
+        return "";
+    }
+
+    function getStatsMultiplier(uint tokenId) private view returns (uint) {
+        if (tokenId == 0 || tokenId == 1) {
+            return 1000;
+        } else if (tokenId == 2) {
+            return 400;
+        } else if (tokenId == 3) {
+            return 225;
+        } else if (tokenId == 4) {
+            return 200;
+        } else if (tokenId == 5) {
+            return 175;
+        } else if (tokenId == 6) {
+            return 150;
+        } else if (tokenId == 7) {
+            return 200;
+        } else if (tokenId == 8) {
+            return 180;
+        } else if (tokenId == 9) {
+            return 160;
+        } else if (tokenId == 10) {
+            return 140;
+        } else if (tokenId == 11) {
+            return 120;
+        } else if (tokenId == 12) {
+            return 100;
+        } else if (tokenId == 13) {
+            return 0;
+        } else if (tokenId == 14) {
+            return 250;
+        } else if (tokenId == 15) {
+            return 225;
+        } else if (tokenId == 16) {
+            return 200;
+        } else if (tokenId == 17) {
+            return 0;
+        }
+        return 0;
+    }
+
+    function getUserTokens(address user) public view returns (uint256[] memory) {
+        uint256 tokenCount = balanceOf(user);
+        if(tokenCount == 0) {
+            return new uint256[](0);
+        }
+        else {
+            uint256[] memory result = new uint256[](tokenCount);
+            uint256 index = 0;
+            for(uint256 i = 0; i < nextId; ++i) {
+                // if owner and not treat
+                if(ownerOf(i) == user) {
+                    result[index++] = i;
+                }
+            }
+            return result;
+        }
+    }
+
+    function buyShiba() public {
+        uint id = uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, nextId))) % 18;
+        uint multiplier = getStatsMultiplier(id);
+
+        uint str = 10 * multiplier + (uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp))) % 6) * multiplier;
+        uint agi = 10 * multiplier + (uint(keccak256(abi.encodePacked(id, block.timestamp))) % 6) * multiplier;
+        uint intl = 10 * multiplier + (uint(keccak256(abi.encodePacked(block.difficulty, id))) % 6) * multiplier;
+
+        mint(id, str / 100, agi / 100, intl / 100);
+    }
+
     function levelUp(uint256 id) public {
+        // level up if power treat
         ++_tokenDetails[id].level;
         _tokenDetails[id].strength += _tokenDetails[id].strengthGain;
         _tokenDetails[id].agility += _tokenDetails[id].agilityGain;
--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -3,7 +3,6 @@ const Token = artifacts.require("Token");
 module.exports = async function (deployer) {
   await deployer.deploy(Token, "Shiba Wars", "SHIBW");
   let tokenInstance = await Token.deployed();
-  await tokenInstance.mint("Bojar da Killa", "Altough he's not a Shiba, do not mess with him. The warden of order.", 100, 100, 100);
-  let shiba = await tokenInstance.getTokenDetails(0);
-  console.log(shiba);
+  await tokenInstance.mint(0, 100, 100, 100);
+  await tokenInstance.mint(1, 100, 100, 100);
 };
