e73b159cc01d6c5b7bac369801391993fa2d02d6
Dominik Krížo <dominik.krizo@doxxbet.sk>
2021-06-04 19:49:11 +0200

    cleanup

---
+++ contracts/DecPort.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract DecPort {
+
+    using SafeMath for uint;
+
+    constructor() {
+
+    }
+
+}
--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -1,27 +1,8 @@
-const ShibaWars = artifacts.require("ShibaWars");
-const ShibaInu = artifacts.require("ShibaInu");
-const STT = artifacts.require("ShibaTreatToken");
-const ShibaWarsFunctions = artifacts.require("ShibaWarsFunctions");
-const ShibaWarsEntity = artifacts.require("ShibaWarsEntity");
+const DecPort = artifacts.require("DecPort");
 
 module.exports = async function (deployer) {
-  // deploy libs
-  await deployer.deploy(ShibaWarsFunctions);
-  await deployer.link(ShibaWarsFunctions, ShibaWars);  
-
-  await deployer.deploy(ShibaWarsEntity);
-  await deployer.link(ShibaWarsEntity, ShibaWars);
-
-  // create shiba wars smart contract
-  await deployer.deploy(ShibaWars);
-  let tokenInstance = await ShibaWars.deployed();
-  await tokenInstance.initialMint();
-
-  // create shiba treat token
-  let shibaWarsAddress = tokenInstance['address'];
-  await deployer.deploy(STT, shibaWarsAddress);
-  tokenInstance = await STT.deployed();
-  await tokenInstance.transferOwnership(shibaWarsAddress);
+  await deployer.deploy(DecPort);
+  let tokenInstance = await DecPort.deployed();
 
   //await deployer.deploy(ShibaInu, "Shiba Inu", "SHIB", 1000000000000000, deployer["networks"]["development"]["from"]);
   //let tokenInstance = await ShibaInu.deployed();
