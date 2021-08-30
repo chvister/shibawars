285445174f4855fdecc1fd3b3b3f8975d2e8e81e
Dominik Krížo <dominik.krizo@doxxbet.sk>
2021-08-31 00:09:04 +0200

    login with metamasks

--- components/NavbarApp.js
+++ components/NavbarApp.js
@@ -2,14 +2,29 @@ import Link from 'next/link'
 import { Button } from "@material-ui/core"
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useMoralis } from 'react-moralis'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import AlertDialog from './AlertDialog'
 
 const NavbarApp = () => {
+    const { authenticate, isAuthenticated, authError, isAuthenticating, logout } = useMoralis();
+
     return (
         <div className={styles.navbar}>
             <div>
                 <Image src="/shibawars_logo_title_new.png" alt="Shibawars" width={1024} height={256}/>
             </div>
             <div>
+                {
+                    authError ? 
+                    <AlertDialog title={"Error"} text={authError.message}/>
+                    : null
+                }
+                {isAuthenticated ? null : 
+                    <Button hidden="true" variant="contained" onClick={() => authenticate() } disabled={isAuthenticating}>
+                        { isAuthenticating ? <CircularProgress size={20}/> : "Login" }
+                    </Button> 
+                }
                 <Link href="/my_dogs" passHref>
                     <Button variant="contained">My Dogs</Button>
                 </Link>
@@ -19,6 +34,7 @@ const NavbarApp = () => {
                 <Link href="/shop" passHref>
                     <Button variant="contained">Shop</Button>
                 </Link>
+                {isAuthenticated ? <Button hidden="true" variant="contained" onClick={() => logout() }>Logout</Button> : null }
             </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import Footer from '../components/Footer'
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
