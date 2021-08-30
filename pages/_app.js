71a7e7ea3944f5a53ba0246312bb629d102e1ff7
Dominik Krížo <dominik.krizo@doxxbet.sk>
2021-08-30 16:56:03 +0200

    add moralis

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
+import { MoralisProvider } from 'react-moralis';
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
@@ -16,18 +17,23 @@ export default function MyApp(props) {
     }
   }, []);
 
-  return (
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
+const appId = "VENnpo7F7P2IjpTpzdSxwbzbJ8XvfsZg8r8P01yC";
+const serverUrl = "https://xmhlcuysesnk.moralis.io:2053/server";
+
+return (
+  <React.Fragment>
+    <Head>
+      <title>My page</title>
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+    </Head>
+    <MoralisProvider appId={appId} serverUrl={serverUrl}>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-    </React.Fragment>
+    </MoralisProvider>
+  </React.Fragment>
   );
 }
 
