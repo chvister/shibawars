39758ad09ae7fad78d2c97f2131236ce87d23a62
Martin Stolar <martin.stolars@gmail.com>
2021-09-10 21:44:39 +0200

    roadmap component

--- components/mainPage/NavbarHeader.js
+++ components/mainPage/NavbarHeader.js
@@ -13,30 +13,29 @@ const NavbarHeader = () => {
             src="/shibawars_head.png"
             className={styles.navbarLogo}
           />
-          {/* <Card.Img
-          variant="top"
-          src="/shibawars_title.png"
-          style={{ width: "120px" }}
-        /> */}
-          {/* <Navbar.Brand>Shibawars</Navbar.Brand> */}
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto" className>
               <div>
-                <Link to="about" className={styles.link + " " + styles.links}>
+                <Link to="about" className={`${styles.link} ${styles.links}`}>
                   About
                 </Link>
               </div>
               <div>
                 <Link
                   to="whitepaper"
-                  className={styles.link + " " + styles.links}
+                  className={`${styles.link} ${styles.links}`}
                 >
                   Whitepaper
                 </Link>
               </div>
               <div>
-                <Link to="team" className={styles.link + " " + styles.links}>
+                <Link to="RoadMap" className={`${styles.link} ${styles.links}`}>
+                  Roadmap
+                </Link>
+              </div>
+              <div>
+                <Link to="team" className={`${styles.link} ${styles.links}`}>
                   Team
                 </Link>
               </div>
---
+++ components/mainPage/Roadmap.js
@@ -0,0 +1,84 @@
+import styles from "../../styles/Home.module.css"
+import { Card } from "react-bootstrap"
+import roadMapStyles from "../../styles/RoadMap.module.css"
+import {
+  LoadingSpinner,
+  GreenCheckbox,
+} from "../customComponents/buttons/LoadingSpinner"
+const RoadMap = () => {
+  return (
+    <>
+      <Card.Img
+        variant="top"
+        src="Roadmap.png"
+        className={styles.titlePhotoAbout}
+      />
+
+      <Card.Body>
+        <h1 className={styles.description}>Q3 2021</h1>
+        <div className={roadMapStyles.timeline}>
+          <div className={`${roadMapStyles.container} ${roadMapStyles.right}`}>
+            <div className={roadMapStyles.content}>
+              <p>
+                Launch web
+                <GreenCheckbox />
+              </p>
+            </div>
+          </div>
+          <div className={`${roadMapStyles.container} ${roadMapStyles.right}`}>
+            <div className={roadMapStyles.content}>
+              <p>
+                Finish the game
+                <LoadingSpinner />
+              </p>
+            </div>
+          </div>
+          <div className={`${roadMapStyles.container} ${roadMapStyles.right}`}>
+            <div className={roadMapStyles.content}>
+              <p>
+                Launch game on testnet
+                <LoadingSpinner />
+              </p>
+            </div>
+          </div>
+          <h1 className={styles.description}>Q4 2021</h1>
+          <div className={`${roadMapStyles.container} ${roadMapStyles.right}`}>
+            <div className={roadMapStyles.content}>
+              <p>
+                Launch ShibaWars season 1
+                <LoadingSpinner />
+              </p>
+            </div>
+          </div>
+          <div className={`${roadMapStyles.container} ${roadMapStyles.right}`}>
+            <div className={roadMapStyles.content}>
+              <p>
+                ShibaWars market place
+                <LoadingSpinner />
+              </p>
+            </div>
+          </div>
+          <div className={`${roadMapStyles.container} ${roadMapStyles.right}`}>
+            <div className={roadMapStyles.content}>
+              <p>
+                Community feedback
+                <LoadingSpinner />
+              </p>
+            </div>
+          </div>
+          <h1 className={styles.description}>Q1 2022</h1>
+          <div className={`${roadMapStyles.container} ${roadMapStyles.right}`}>
+            <div className={roadMapStyles.content}>
+              <p>
+                Launch ShibaWars season 2
+                <LoadingSpinner />
+              </p>
+            </div>
+          </div>
+        </div>
+      </Card.Body>
+    </>
+  )
+}
+
+export default RoadMap
