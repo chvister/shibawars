edcb75134f0572299af2a87449d0bd021912bf7d
Martin Stolar <martin.stolars@gmail.com>
2021-09-17 12:36:35 +0200

    addede photos & small details

--- components/mainPage/About.js
+++ components/mainPage/About.js
@@ -1,15 +1,34 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import styles from "../../styles/Home.module.css"
-import { Card } from "react-bootstrap"
+import { Card, Modal, Row, Col } from "react-bootstrap"
 import AOS from "aos"
 import "aos/dist/aos.css"
 
 const About = () => {
+  const [show, setShow] = useState(false)
+  const [targetPicture, setTargetPicture] = useState("")
+
+  const dogImages = ["dog1.jpg", "dog2.png"]
+
+  const handleClose = () => setShow(false)
+
+  const handleShow = (e) => {
+    var index = e.toString().lastIndexOf("/") + 1
+    var filename = e.substr(index)
+    setShow(true)
+    setTargetPicture(filename)
+  }
+
+  const handleShowDialog = () => {
+    setShow(!show)
+  }
+
   useEffect(() => {
     AOS.init({
       duration: 800,
     })
   }, [])
+
   return (
     <div style={{ marginTop: "5rem" }}>
       <Card.Img
@@ -30,6 +49,50 @@ const About = () => {
         Pack with a little bit of luck and some are only awarded for OG Shiba
         Inu Token holders.
       </Card.Body>
+      <div className={styles.borderCards}>
+        <Row className={styles.dogsAlbumRow}>
+          {dogImages.map((value, index) => {
+            return (
+              <Col>
+                <Card.Img
+                  data-aos="fade-up"
+                  className={styles.dogsAlbum}
+                  variant="top"
+                  key={index}
+                  src={value}
+                  alt={value}
+                  onClick={(e) => handleShow(e.target.currentSrc)}
+                />
+              </Col>
+            )
+          })}
+        </Row>
+      </div>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        animation={false}
+        style={{ marginTop: "0 auto" }}
+      >
+        <Modal.Body closeButton className={styles.modalWindow}>
+          <>
+            <button
+              type="button"
+              class="btn-close"
+              aria-label="Close"
+              style={{ position: "absolute" }}
+              onClick={handleShowDialog}
+            ></button>
+
+            <img
+              className={styles.ModelDogImg}
+              src={targetPicture}
+              onClick={handleShowDialog}
+              alt="no image"
+            />
+          </>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
